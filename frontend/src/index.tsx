17231b6302c2aa3535586c65ad51b962c05ca68c
Paige Maute <p.maute@obscuritylabs.com>
2022-07-14 03:49:46 +0000

    chore: frontend init

---
+++ frontend/src/index.tsx
@@ -0,0 +1,32 @@
+// Copyright (C) 2022 Obscurity Labs LLC. <admin@obscuritylabs.com> - All Rights Reserved
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// All rights reserved. No warranty, explicit or implicit, provided.
+// Proprietary and confidential.
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+// import 'react-quill/dist/quill.snow.css';
+import 'nprogress/nprogress.css';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { BrowserRouter } from 'react-router-dom';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
+import App from './App';
+import React from 'react';
+
+ReactDOM.render(
+  <StrictMode>
+    <HelmetProvider>
+      <StyledEngineProvider injectFirst>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </LocalizationProvider>
+      </StyledEngineProvider>
+    </HelmetProvider>
+  </StrictMode>,
+  document.getElementById('root')
+);
