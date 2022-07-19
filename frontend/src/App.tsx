50350d7881b151dd88ca94d1d84e3bde6c5b8c10
Pierre Tran <pierretranva@gmail.com>
2022-07-19 00:25:09 +0000

    feat: added new frontend items and output for fetch to api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,28 +4,52 @@
 // Proprietary and confidential.
 import React, { useEffect, useState } from "react";
 import type { FC } from 'react';
-import { Button, Dialog, Typography } from '@material-ui/core';
+import { Button, Dialog, Typography, DialogTitle, DialogContent } from '@material-ui/core';
+import { Counter } from "./Counter"
 
 const App: FC = () => {
-  const [data, setData] = useState([])
-  const [open, setOpen] = React.useState(false);
+  const [data, setData] = useState<string>()
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [veggie, setVeggie] = useState<string>()
   const fetchData = async () => {
     const response = await fetch("http://localhost:8000/hello")
     const jsonRes = await response.json()
-    setData(jsonRes.data)
-    setOpen(!open)
+    console.log(jsonRes)
+    setData(jsonRes)
+    console.log("it is working rght here")
+    console.log(jsonRes)
   }
 
   const handleClose = () => {
     setOpen(false)
   }
+  const handleClickVeggie = (rand: number) => {
+    let randomVeg = ["potato", "onion", "lettuce", "tomato"]
+    setVeggie(randomVeg[rand])
+    console.log(randomVeg[rand])
+  }
 
   return (
-    <><Button onChange={fetchData}>Hello World</Button><Dialog
-      open={open}>
-      onClose={handleClose}
-      <Typography>{data}</Typography>
-    </Dialog></>
+    <div>
+      <Button onClick={fetchData}>Hello World</Button>
+      <Typography>{JSON.stringify(data)}</Typography>
+      <h1>Pick a random number to get a Veggie</h1>
+      <Button onClick={() => handleClickVeggie(0)}>Veggie 1</Button>
+      <Button onClick={() => handleClickVeggie(1)}>Veggie 2</Button>
+      <Button onClick={() => handleClickVeggie(2)}>Veggie 3</Button>
+      <Button onClick={() => handleClickVeggie(3)}>Veggie 4</Button>
+      <Typography>{veggie}</Typography>
+      <Counter>
+        {(count, setCount) =>
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+
+          </div>}
+      </Counter>
+
+    </div>
+
   );
 };
 
