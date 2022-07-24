c30b157c66a5ce92fed0b8d85567c1e5465dc5bd
Pierre Tran <pierretranva@gmail.com>
2022-07-24 04:52:15 +0000

    feat: restructured app and built out ShoppingCart Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,42 +4,25 @@
 // Proprietary and confidential.
 import React, { useEffect, useState } from "react";
 import type { FC } from 'react';
-import { Button, Dialog, Typography, TextField, Skeleton, HomeIcon } from '@material-ui/core';
+import { Button, Dialog, Typography, TextField, Skeleton } from '@material-ui/core';
 import { Counter } from "./Counter"
 import { ShoppingCart } from "./ShoppingCart"
 
 const App: FC = () => {
   const [data, setData] = useState<string>()
   const [open, setOpen] = React.useState<boolean>(false);
-  const [veggie, setVeggie] = useState<string>()
   const fetchData = async () => {
     const response = await fetch("http://localhost:8000/hello")
     const jsonRes = await response.json()
-    setData(jsonRes)
+    setData(String(jsonRes))
 
   }
 
   const handleClose = () => {
     setOpen(false)
   }
-  const handleClickVeggie = (rand: number) => {
-    let veg = ["potato", "onion", "lettuce", "tomato", "death"]
-    setVeggie(veg[rand])
-    console.log(veg[rand])
-
-  }
-
   return (
     <div>
-      <Button onClick={fetchData}>Hello World</Button>
-
-      <h1>Pick a random number to get a Veggie</h1>
-      <Button onClick={() => handleClickVeggie(0)}>Veggie 1</Button>
-      <Button onClick={() => handleClickVeggie(1)}>Veggie 2</Button>
-      <Button onClick={() => handleClickVeggie(2)}>Veggie 3</Button>
-      <Button onClick={() => handleClickVeggie(3)}>Veggie 4</Button>
-      <Button onClick={() => handleClickVeggie(4)}>DON'T CLICK THIS BUTTON UNLESS YOU WANT THE WORLD TO END</Button>
-      <Typography>{veggie}</Typography>
       <Counter>
         {(count, setCount) =>
           <div>
@@ -49,20 +32,9 @@ const App: FC = () => {
 
           </div>}
       </Counter>
-      <Skeleton variant="text" />
-      <Skeleton variant="circular" width={40} height={40} />
-      <Skeleton variant="rectangular" width={210} height={118} />
-      <ShoppingCart>
-        {(cart, modifyCart) =>
-          <div>
-            <TextField size="small" />
-            {cart.name}{cart.count}{modifyCart}
 
-            <button onClick={() => modifyCart({ name: cart.name, count: cart.count + 1 })}>+</button>
-            <button onClick={() => modifyCart({ name: cart.name, count: cart.count - 1 })}>-</button>
-          </div>
-        }
-      </ShoppingCart>
+      <ShoppingCart />
+
 
     </div>
 
--- frontend/src/ShoppingCart.tsx
+++ frontend/src/ShoppingCart.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button, Dialog, Typography, TextField, Skeleton } from '@material-ui/core';
 
 interface Props {
     children: (
@@ -15,14 +16,28 @@ var item = {
 };
 
 
+export const ShoppingCart: React.FC = () => {
+    const [cart, modifyCart] = useState({ name: "apple", count: 20 });
 
-export const ShoppingCart: React.FC<Props> = ({ children }) => {
-    const [cart, modifyCart] = useState({ name: "", count: 0 });
+    const handleClickVeggie = (rand: number) => {
+        let veg = ["potato", "onion", "lettuce", "tomato"]
+        modifyCart({ name: veg[rand], count: cart.count })
+        console.log(veg[rand])
 
-    return (<div>
-        {
-        children(cart, modifyCart)
-        }
-        </div>;)
+    }
 
+    return (
+
+        <div>
+            <h1>Choose your veggie and add it to the cart</h1>
+            <Typography>{cart.name}{cart.count}{modifyCart}</Typography>
+            <Button onClick={() => handleClickVeggie(0)}>Potato</Button>
+            <Button onClick={() => handleClickVeggie(1)}>Onion</Button>
+            <Button onClick={() => handleClickVeggie(2)}>Lettuce</Button>
+            <Button onClick={() => handleClickVeggie(3)}>Tomato</Button>
+            <button onClick={() => modifyCart({ name: cart.name, count: cart.count + 1 })}>+</button>
+            <button onClick={() => modifyCart({ name: cart.name, count: cart.count - 1 })}>-</button>
+        </div>
+
+    );
 };
