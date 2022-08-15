3474705f30900658b39c6ac00f5d4fc2201f9c2f
Pierre Tran <pierretranva@gmail.com>
2022-08-15 02:49:59 +0000

    feat: complete about section and added image to about section

--- frontend/src/components/aboutMe/About.tsx
+++ frontend/src/components/aboutMe/About.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Button, Typography, Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Typography, Divider, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
 import { ArrowRightSharp } from '@material-ui/icons'
-import icon_128 from "../../assets/icon_128.png"
+import picture from "../../assets/picture.jpeg"
 import './About.css'
 function generateListItem() {
     const technologies: Array<String> = ["Python", "Java", "HTML", "CSS", "Javascript", "Typescript", "SQL", "Bash"]
@@ -23,27 +23,29 @@ const About = () => {
 
     return (
         <div className="about__main">
-            <div className="about__main-title">
-                <Divider textAlign="center" role="presentation" >
-                    <Typography align="center" variant="h2">About Me</Typography>
-                </Divider>
-            </div>
-            <div className="about__main-body">
-                <Typography align="left" variant="body2">Hello! My name is Pierre and I am an aspiring Software Engineer. I am currently studying computer science at Virginia Tech. My past experience in software engineeer comes from my love of eating food. </Typography>
-                <Typography align="left" variant="body2">Software Engieer </Typography>
-            </div>
-            <div className="about__main-tech">
-                <Typography align="center" variant="body2">Heres a few programming languages I've worked with in the past: </Typography>
-                <div className="about__main-tech_list">
 
-                    <List dense={true} sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
-                        {generateListItem()}
-
-                    </List>
-
+            <div className="about__main-text">
+                <div className="about__main-title">
+                    <Divider textAlign="center" role="presentation" >
+                        <Typography align="center" variant="h6">About Me</Typography>
+                    </Divider>
                 </div>
 
-
+                <div className="about__main-body">
+                    <Typography align="left" variant="body2">Hello! My name is Pierre and I am an aspiring Software Engineer. I am currently studying computer science at Virginia Tech. My past experience in software engineeer comes from my love of eating food. </Typography>
+                    <Typography align="left" variant="body2">Software Engieer </Typography>
+                </div>
+                <div className="about__main-tech">
+                    <Typography align="center" variant="body2">Heres a few programming languages I've worked with in the past: </Typography>
+                    <div className="about__main-tech_list">
+                        <List dense={true} sx={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)' }}>
+                            {generateListItem()}
+                        </List>
+                    </div>
+                </div>
+            </div>
+            <div className="about__main-image">
+                <img src={picture} className="image" />
             </div>
 
         </div >
